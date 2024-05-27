804f35423a8411a992342c277a1e7acc5cb21454
Davyd <allbuddyes@gmail.com>
2024-05-27 10:36:00 +0300

    a

--- src/MyLinkedList.java
+++
@@ -1,8 +0,0 @@
-public interface MyLinkedList <A extends Comparable<A>> extends Iterable<A>{
-    void addtofront(A element);
-    void addtoback(A element);
-    void delete(int index);
-    void delete(A element);
-    A get(int index);
-    int size();
-}
--- src/MyList.java
+++
@@ -1,7 +0,0 @@
-public interface MyList <T extends Comparable<T>> extends Iterable<T>{
-    void add(T element);
-    void delete(int index);
-    void delete(T element);
-    T get(int index);
-    int size();
-}
